7fbfb85d7fc23fa5b9c15f40c3a6681526f90aba
Anhony Webb <anthony.webb@gmail.com>
2013-08-24 02:38:04 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Node Sprinkler
+
+A node.js application to enable a BeagleBone to become a smart sprinkler controller
+
+## Installation
+
+To install simple copy config-template.json to a new file called config.json
+Install depedencies: npm install
+To run, simply type: node server.js
+
+## Usage
+
+This project acts as an API to our forthcoming mobile app.  If you wish to play around with it without the app you may load the postman collection (postman.js) into the postman chrome extension and browse the methods that are exposed in the API.
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## History
+
+TODO: Write history
+
+## Credits
+
+TODO: Write credits
+
+## License
+
+TODO: Write license
---
+++ server.js
@@ -0,0 +1,216 @@
+var fs = require('fs');
+var dgram = require('dgram');
+var express = require('express');
+var moment = require('moment-timezone');
+
+///////////////////////////////////////
+// LOAD THE DEFAULT CONFIG
+//////////////////////////////////////
+var config = fs.readFileSync('./config.json');
+try {
+    config = JSON.parse(config);
+    //console.log(config);
+}
+catch (err) {
+    console.log('There has been an error parsing your config')
+    console.log(err);
+} 
+
+// Some of our default vars
+var t, i;
+var running = {};
+var runqueue = [];
+var zonecount = config.zones.length;
+var programcount = config.zones.length;
+
+///////////////////////////////////////
+// CONFFIGURE THE WEBSERVER
+//////////////////////////////////////
+var app = express();
+app.use(express.favicon());
+app.use(express.bodyParser());
+app.use(app.router);
+app.use(missingHandler);
+
+// Routes
+app.get('/config', function(req, res){
+    res.json(config);
+});
+
+app.post('/config', function(req, res){
+    console.log(req.body);
+
+    var data = JSON.stringify(req.body);
+
+    fs.writeFile('./config.json', data, function (err) {
+        if (err) {
+            console.log('There has been an error saving your configuration data.');
+            console.log(err.message);
+            return;
+        }
+        console.log('Configuration saved successfully.');
+        config = req.body;
+        zonecount = config.zones.length;
+        programcount = config.zones.length;
+    });
+
+    res.json({status:'ok',msg:'config saved'});
+});
+
+app.get('/status', function(req, res){
+    res.json({status:'ok',running:running,queue:runqueue});
+});
+
+app.get('/off', function(req, res){
+    zonesOff(true);
+    res.json({status:'ok',msg:'all zones have been turned off'});
+});
+
+app.get('/zone/:id/on/:seconds', function(req, res){
+    var zoneindex = req.params.id-1;
+    if(zoneindex>=0 && zoneindex<zonecount-1){
+        zoneOn(zoneindex,req.params.seconds);
+        res.json({status:'ok',msg:'started zone: '+config.zones[zoneindex].name});    
+    }
+    else {
+        errorHandler(res,'That is not a valid zone')
+    }
+});
+
+app.get('/program/:id/on', function(req, res){
+    var programindex = req.params.id-1;
+    if(programindex>=0 && programindex<programcount-1){
+        programOn(programindex);
+        res.json({status:'ok',msg:'started program: '+config.programs[programindex].name});    
+    }
+    else {
+        errorHandler(res,'That is not a valid program');
+    }
+});
+
+///////////////////////////////////////
+// START UP THE APP
+//////////////////////////////////////
+app.listen(config.webserver.port);
+console.log('Listening on port '+config.webserver.port);
+
+// Add the listener for recurring program schedules
+setInterval(function(){
+    var d = moment().tz(config.timezone);
+    var currTime = d.format('HH:mm');
+    var currDay = parseInt(d.format('d'));
+
+    // loop over the programs and see if we have a match
+    for(var i=0;i<config.programs.length;i++){
+        if(config.programs[i].active && currTime == config.programs[i].start &&  config.programs[i].days.indexOf(currDay) != -1){
+            programOn(i);
+        }
+    }
+    
+},60000) 
+
+
+// Start auto discovery UDP broadcast ping
+var message = new Buffer("Some bytes");
+var socket = dgram.createSocket("udp4");
+socket.bind();
+socket.setBroadcast(true);
+
+setInterval(function(){
+    socket.send(message, 0, message.length, 41234, '255.255.255.255', function(err, bytes) {
+        if(err){
+            console.log(err);
+        }
+    });
+            
+},6000);
+
+
+///////////////////////////////////////
+// HELPERS
+//////////////////////////////////////
+function missingHandler(req, res, next) {
+    console.log('404 Not found - '+req.url);
+    res.json(404, { status: 'error', msg: 'Not found, sorry...' });
+}
+
+function errorHandler(res, msg) {
+    console.log(err);
+    res.json(500, { status: 'error', msg: msg });
+}
+
+function clearTimers() {
+    clearInterval(i);
+    clearTimeout(t);
+}
+
+function resetCounts() {
+    zonecount = config.zones.length;
+    programcount = config.zones.length;
+}
+
+function zoneOn(index,seconds) {
+    zonesOff(true);
+    runqueue.push({zone:index+1,seconds:seconds});
+    processQueue();
+}
+
+function programOn(index) {
+    zonesOff(true);
+    runqueue = config.programs[index].zones;
+    processQueue();
+}
+
+function zonesOff(killqueue) {
+    // shut down all the zones
+    console.log('shutting off all zones');
+    running = {};
+
+    if(killqueue){
+        console.log('clearing the queue');
+        runqueue = [];
+        // kill any outstanding timers
+        clearTimers();
+    }
+}
+
+function processQueue() {
+    // is anything in the queue?
+    if(runqueue.length){
+        // start working on the next item in the queue
+        running = runqueue.shift();
+        running.remaining = running.seconds;
+        console.log('Starting zone '+running.zone+' for '+running.seconds+' seconds');
+        
+        if(running.seconds > 0) {
+            // clear any timers that are currently running
+            clearTimers();
+
+            // count down the time remaining
+            i = setInterval(function(){
+                if(running.zone){
+                    running.remaining = running.remaining-1;
+                }
+                
+            },1000) 
+
+            // start a countdown timer for the zone watering time
+            t = setTimeout(function(){
+                // turn off the zones, pass false so it wont kill the rest of the items in the queue
+                console.log('Done with zone '+running.zone+' for '+running.seconds+' seconds');
+                zonesOff(false);
+
+                // wait a couple seconds and kick off the next
+                setTimeout(function(){
+                    processQueue();
+                },2000) 
+
+            },running.seconds*1000)    
+        }
+
+    } else {
+        // once there is nothing left to process we can clear the timers
+        clearTimers();
+
+    }
+}
